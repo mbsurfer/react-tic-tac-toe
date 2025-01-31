160b9818573abd075d94d2520146f151718b7c78
mbsurfer <rneldner@gmail.com>
2025-01-31 12:23:10 -0800

    create winning state and rematch

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({name, symbol, isActive, onChangeName}) {
     const [isEditing, setIsEditing] = useState(false);
     const [nameValue, setNameValue] = useState(name)
 
@@ -12,18 +12,23 @@ export default function Player({name, symbol}) {
         * the initial render.
         * */
         setIsEditing(editing => !editing);
+
+        if (isEditing) {
+            onChangeName(symbol, nameValue);
+        }
     }
 
+    /* This function is being called for every keystroke in the input field */
     function handleChange(event) {
         setNameValue(event.target.value);
     }
 
     const playerName = (isEditing) ?
-        <input type="text" placeholder="Player Name" value={nameValue} onChange={handleChange} /> :
+        <input type="text" placeholder="Player Name" value={nameValue} onChange={handleChange}/> :
         <span className="player-name">{nameValue}</span>;
 
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
               <span className="player">
                   {playerName}
                   <span className="player-symbol">{symbol}</span>
